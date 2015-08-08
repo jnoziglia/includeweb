608546334f613c07ff85f4739b2b2f963f018a27
mpandolfelli <martinpandolfelli@gmail.com>
2015-08-08 19:38:30 -0300

    agregado parallax, animacion, y giladas

--- app.js
+++ app.js
@@ -10,11 +10,13 @@ router.get('/', function(req, res) {
 
 app.use(express.static('node_modules/materialize-css/bin'));
 app.use(express.static('node_modules/materialize-css/font'));
+app.use(express.static('css/'));
+app.use(express.static('images/'));
 
 app.use('/', router);
 
 var server = require('http').Server(app);
 
-server.listen(8080, function() {
+server.listen(8010, function() {
   console.log("Node server running on http://localhost:8000");
 });
