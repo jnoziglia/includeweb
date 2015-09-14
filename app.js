909fcecbc58527c857daf7aed1049cde5a177dc0
jnoziglia <julian.noziglia@gmail.com>
2015-09-14 00:27:49 -0300

    Agrego respuesta del post

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ var express = require("express"),
 
 var router = express.Router();
 
-
 router.get('/', function(req, res) {
 	res.sendFile(__dirname + '/views/indexx.html');
 });
 router.get('/oscuro', function(req, res) {
 	res.sendFile(__dirname + '/views/oscuro.html');
 });
+
+router.get('/contact', contact);
 
 app.use(express.static('node_modules/materialize-css/bin'));
 app.use(express.static('node_modules/materialize-css/font'));
@@ -24,3 +25,10 @@ var server = require('http').Server(app);
 server.listen(8010, function() {
   console.log("Node server running on http://localhost:8010");
 });
+
+
+function contact(req, res) {
+	console.log(req.query.nombre);
+	console.log(req.query.mail);
+	console.log(req.query.mensaje);
+}
